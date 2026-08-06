08cb45fcef937202625602ba00a6e8022f0cf03c
sainivedbainagar-source <sainivedbainagar@gmail.com>
2026-08-06 21:56:30 +0530

    Add LeetCode #125 Valid Palindrome

--- LeetCode/Strings/125_Valid_Palindrome.cpp
+++ LeetCode/Strings/125_Valid_Palindrome.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
-    void reverseString(vector<char>& s) {
-        int i = 0;
-        int j = s.size() - 1;
-        while ( i < j)
-        {
-            swap(s[i],s[j]);
-            i++;
-            j--;
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.size() - 1;
+
+        while (left < right) {
+            if (!isalnum(s[left])) {
+                left++;
+            } else if (!isalnum(s[right])) {
+                right--;
+            } else {
+                if (tolower(s[left]) != tolower(s[right]))
+                    return false;
+
+                left++;
+                right--;
+            }
         }
+        return true;
     }
 };

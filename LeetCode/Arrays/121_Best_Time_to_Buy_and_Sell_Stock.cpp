6994e6c4625ffa6664cc80219d3f3a1dd78ae277
sainivedbainagar-source <sainivedbainagar@gmail.com>
2026-08-14 22:27:40 +0530

    Add 121_Best_Time_to_Buy_and_Sell_Stock.cpp

--- LeetCode/Arrays/121_Best_Time_to_Buy_and_Sell_Stock.cpp
+++ LeetCode/Arrays/121_Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -9,20 +9,21 @@ Space Complexity: O(1)
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int min_price = INT_MAX;
-        int max_profit = 0;
-        
-        for (int price : prices) {
-            
-            if (price < min_price) {
-                min_price = price;
-            }
-            
-            else if (price - min_price > max_profit) {
-                max_profit = price - min_price;
-            }
+        int p = 0;
+        int min = prices[0];
+        for(int i = 0; i<prices.size();i++){
+          if( prices[i]<min){
+            min = prices[i];
+          }
+          int x = prices[i]-min;
+          if(x>p){
+            p= x;
+          }
+
         }
-        
+        return p;
+    }
+};
         return max_profit;
     }
 };

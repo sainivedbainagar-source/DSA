51d1332facd117d44ebfe9d45b4e43d2f70648e3
sainivedbainagar-source <sainivedbainagar@gmail.com>
2026-09-03 20:29:57 +0530

    Add LeetCode #20 Valid Parentheses

---
+++ LeetCode/Strings/020_Valid_Parentheses.cpp
@@ -0,0 +1,30 @@
+/*
+LeetCode 20 - Valid Parentheses
+Difficulty: Easy
+
+Approach: Stack
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char c : s) {
+            if (c == '(')
+                st.push(')');
+            else if (c == '[')
+                st.push(']');
+            else if (c == '{')
+                st.push('}');
+            else {
+                if (st.empty() || st.top() != c)
+                    return false;
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};

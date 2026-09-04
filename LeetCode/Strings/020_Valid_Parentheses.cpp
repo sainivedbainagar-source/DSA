fabae37bb5663c723b0a76ded850d2bbe74cdc6d
sainivedbainagar-source <sainivedbainagar@gmail.com>
2026-09-04 10:25:44 +0530

    Add 020_Valid_Parentheses.cpp

--- LeetCode/Strings/020_Valid_Parentheses.cpp
+++ LeetCode/Strings/020_Valid_Parentheses.cpp
@@ -9,22 +9,24 @@ Space Complexity: O(n)
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-
-        for (char c : s) {
-            if (c == '(')
-                st.push(')');
-            else if (c == '[')
-                st.push(']');
-            else if (c == '{')
-                st.push('}');
-            else {
-                if (st.empty() || st.top() != c)
-                    return false;
-                st.pop();
+        stack <char> st;
+        char ch ;
+    int i = 0;
+         while(  i<s.size()){
+            ch = s[i];
+            if( ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
             }
-        }
-
-        return st.empty();
+            else {
+                 if( st.empty())
+                  return false;
+                  if ( ch == ')' && st.top() != '(' || ch == ']' && st.top() != '[' ||  ch == '}' && st.top() != '{'   )
+                   return false;
+                   st.pop();
+            }
+            i++;
+         }
+         return st.empty();
     }
+};
 };
